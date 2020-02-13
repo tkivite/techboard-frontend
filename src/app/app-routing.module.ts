725f9c7abf2d3560fb04d0f94f1337cb6f472224
titus <tkivite@gmail.com>
2020-02-13 11:43:52 +0300

    added placeholding content for contact us and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
-const routes: Routes = [];
+import { LoginComponent } from "./pages/login/login.component";
+import { UsersComponent } from "./pages/users/users.component";
+import { ContactusComponent } from "./pages/contactus/contactus.component";
+import { CreateComponent } from "./pages/users/create/create.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "users", component: UsersComponent },
+  { path: "users/create", component: CreateComponent },
+  { path: "contactus", component: ContactusComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { UsersComponent } from './pages/users/users.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { CreateComponent } from './pages/users/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    UsersComponent
+    UsersComponent,
+    ContactusComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
