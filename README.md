3bc49f513fa1b323be152a009de0c27247f2e795
titus <tkivite@gmail.com>
2020-02-13 11:11:40 +0300

    added styling and inner pages

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 > 3. cd techboard <br>
 > 4. ng serve <br>
 
+# Other required packages
+
+> 1. npm install bootstrap@4.4.1
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
